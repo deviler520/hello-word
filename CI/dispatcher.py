101033a2aa7696e03e1364ecd770e0eb61045637
gaopeng <devil_er@163.com>
2019-11-06 23:31:56 +0800

    feat:(dispatcher) socket server

--- CI/dispatcher.py
+++ CI/dispatcher.py
@@ -3,28 +3,41 @@
 import socketserver
 import re
 
+from pip import __main__
+
 
 class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     runners = []
-    dead = false
+    dead = False
     dispatched_commits = {}
     pending_commits = []
 
 
 class RequestHandler(socketserver.BaseRequestHandler):
-
     BUF_SIZE = 1024
     command_re = re.compile(r"([a-z]+):?(.*)")
 
     def handle(self):
-        receive_data = self.request.recv(self.BUF_SIZE).trip()
+        receive_data = self.request.recv(self.BUF_SIZE).decode().strip()
+        print("receive_data %s" % receive_data)
         command_group = self.command_re.match(receive_data)
         if not command_group:
             self.request.sendall("Invalid command")
-            return 
+            return
         command = command_group.group(1)
         print("receive command %s" % command)
         if command == "status":
-            self.request.sendall("OK")
-        elif command == "register":
-            pass
+            self.request.sendall(b"status OK")
+        elif command == "register":  # "register:127.0.0.1:8080"
+            host, port = command_group(2).split(":")
+            self.server.runners.append({"host": host, "port": port})
+            self.request.sendall(b"register OK")
+
+
+def server(host, port):
+    dispatch_server = ThreadingTCPServer((host, port), RequestHandler)
+    dispatch_server.serve_forever()
+
+
+if __name__ == "__main__":
+    server("127.0.0.1", 8080)
--- CI/helper.py
+++ CI/helper.py
@@ -2,11 +2,15 @@
 
 
 import os
+import socket
 
 
 def communicate(dispatcher_host, dispatcher_port, command):
-    if "status" == command:
-        return "OK"
-    else:
-        pass
-    return "OK"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((dispatcher_host, dispatcher_port))
+    s.sendall(command)
+    print(s.recv(1024))
+
+
+if __name__ == "__main__":
+    communicate("127.0.0.1", 8080, b"status")
